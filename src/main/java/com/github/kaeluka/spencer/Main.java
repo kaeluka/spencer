56b78be0c790e4724fe5e2f5280dca7ff5f12395
Stephan Brandauer <stephan.brandauer@it.uu.se>
2016-06-21 00:04:09 +0200

    clean up generation argument string

--- src/main/java/com/github/kaeluka/spencer/Main.java
+++ src/main/java/com/github/kaeluka/spencer/Main.java
@@ -3,7 +3,11 @@ import com.github.kaeluka.spencer.server.TransformerServer;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.net.URLClassLoader;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -25,19 +29,24 @@ public class Main {
     }
 
     private static Process getProcess(final String[] args) throws IOException {
-        String sep = System.getProperty("file.separator");
+        final String sep = System.getProperty("file.separator");
         final String path = System.getProperty("java.home") + sep + "bin" + sep + "java";
-        final String argString = StringUtils.join(Arrays.asList(args), " ");
-//        final List<URL> urLs = Arrays.asList(((URLClassLoader) Main.class.getClassLoader()).getURLs());
-//        System.out.println("class path: "+ urLs);
-        //FIXME hard coded paths
+        final ArrayList<String> argStrings = new ArrayList();
+        final List<URL> urLs = Arrays.asList(((URLClassLoader) Main.class.getClassLoader()).getURLs());
+        System.out.println("class path: "+ urLs);
+        String cpPrepend =
+                // the native interface class:
+                ":"+System.getProperty("user.home")+("/.m2/repository/com/github/kaeluka/spencer-tracing-java/0.1.2-SNAPSHOT/spencer-tracing-java-0.1.2-SNAPSHOT.jar").replaceAll("/", sep)+
+                // the transformed runtim
+                ":"+System.getProperty("user.home")+("/.spencer/instrumented_java_rt/output".replaceAll("/", sep));
+        argStrings.add(path);
+        argStrings.add("-Xbootclasspath/p"+cpPrepend);
+        argStrings.add("-agentpath:"+System.getProperty("user.home")+("/.m2/repository/com/github/kaeluka/spencer-tracing-jni/0.1.2-SNAPSHOT/spencer-tracing-jni-0.1.2-SNAPSHOT.so=tracefile=/tmp/tracefile".replaceAll("/", sep)));
+        argStrings.addAll(Arrays.asList(args));
         ProcessBuilder processBuilder = new ProcessBuilder(
-                path,
-                "-Xbootclasspath/p:/Users/stebr742/.m2/repository/com/github/kaeluka/spencer-tracing-java/0.1.2-SNAPSHOT/spencer-tracing-java-0.1.2-SNAPSHOT.jar",
-                "-agentpath:/Users/stebr742/.m2/repository/com/github/kaeluka/spencer-tracing-jni/0.1.2-SNAPSHOT/spencer-tracing-jni-0.1.2-SNAPSHOT.so=tracefile=/tmp/tracefile",
-                argString);
+                argStrings);
 
-        System.out.println(processBuilder.command());
+        System.out.println("running command: "+processBuilder.command());
 
         processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
         processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
